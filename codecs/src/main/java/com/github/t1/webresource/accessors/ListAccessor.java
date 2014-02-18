9589af02f138ee51d1a9d49cb20af5adac8aea19
Rüdiger zu Dohna <snackbox@sinntr.eu>
2014-02-18 08:37:16 +0100

    add ListAccessor

---
+++ codecs/src/main/java/com/github/t1/webresource/accessors/ListAccessor.java
@@ -0,0 +1,11 @@
+package com.github.t1.webresource.accessors;
+
+import java.net.URI;
+import java.util.List;
+
+public class ListAccessor extends AbstractAccessor<List<?>> {
+    @Override
+    public URI link(List<?> element) {
+        return null; // TODO put into ListMetaData
+    }
+}
